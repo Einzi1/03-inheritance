a7acb17050cd0db6025a5a53b180d7d8ca2b2a88
Einzi1 <einzinger.simon@googlemail.com>
2019-04-15 19:04:49 +0200

    lösung impl

---
+++ src/main/java/ohm/softa/a03/State.java
@@ -0,0 +1,40 @@
+package ohm.softa.a03;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class State {
+
+    protected static final Logger logger = LogManager.getLogger();
+    private int t = 0;
+    private final int duration;
+
+    public State(int duration) {
+        this.duration = duration;
+    }
+
+    final State tick(Cat cat) {
+        if (duration < 0) {
+            return this;
+        }
+
+        t = t + 1;
+
+        if (t < duration) {
+            logger.info("Still in {}", getClass().getSimpleName());
+            return this;
+        } else {
+            return successor(cat);
+        }
+    }
+
+    public abstract State successor(Cat cat);
+
+    public int getTime() {
+        return t;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
---
+++ src/main/java/ohm/softa/a03/states/HungryState.java
@@ -0,0 +1,22 @@
+package ohm.softa.a03.states;
+
+import ohm.softa.a03.Cat;
+import ohm.softa.a03.State;
+
+public class HungryState extends State {
+
+    public HungryState(int duration) {
+        super(duration);
+    }
+
+    @Override
+    public State successor(Cat cat) {
+        logger.info("I've starved for a too long time...good bye...");
+        return new DeathState();
+    }
+
+    public State feed(Cat cat) {
+        logger.info("You feed the cat...");
+        return new DigestingState(cat.getDigest(), getDuration() - getTime());
+    }
+}
